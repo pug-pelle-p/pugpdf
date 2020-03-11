31ae1439f1281c83f61eb3f674411cf836146db5
Pelle Branden <pelle@froda.se>
2020-03-11 13:40:13 +0100

    Changed default margin to map to wkhtmltopdfs defaults

--- PugPdf.Core/PdfPrintOptions.cs
+++ PugPdf.Core/PdfPrintOptions.cs
@@ -2,10 +2,10 @@ namespace PugPdf.Core
 {
     public class PdfPrintOptions
     {
-        public double MarginLeft { get; set; } = 0d;
-        public double MarginRight { get; set; } = 0d;
-        public double MarginTop { get; set; } = 0d;
-        public double MarginBottom { get; set; } = 0d;
+        public double MarginLeft { get; set; } = 10d;
+        public double MarginRight { get; set; } = 10d;
+        public double MarginTop { get; set; } = 10d;
+        public double MarginBottom { get; set; } = 10d;
         public PdfPageSize PageSize { get; set; } = PdfPageSize.A4;
         public PdfOrientation Orientation { get; set; } = PdfOrientation.Portrait;
         public string Title { get; set; }
