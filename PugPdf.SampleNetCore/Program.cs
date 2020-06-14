11d265bfd1af1f938f87b6c18eb184068af2dee5
Pelle Branden <pelle@froda.se>
2020-06-14 12:25:17 +0200

    Added image example

--- PugPdf.SampleNetCore/Program.cs
+++ PugPdf.SampleNetCore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using PugPdf.Core;
 
@@ -13,6 +14,42 @@ namespace PugPdf.SampleNetCore
             renderer.PrintOptions.Title = "My title";
             
             var pdf = await renderer.RenderHtmlAsPdfAsync("<h1>Hello world</h1>");
+
+            await pdf.SaveAsAsync("c:\\my.pdf");
+            
+            var filePath = await pdf.SaveInTempFolderAsync();
+
+            Console.WriteLine(filePath);
+            
+            Console.ReadLine();
+        }
+
+        static async Task PrintWithImageFromUrl()
+        {
+            var renderer = new HtmlToPdf();
+
+            renderer.PrintOptions.Title = "My title";
+            
+            var pdf = await renderer.RenderHtmlAsPdfAsync("<img src=\"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png\"><h1>Hello world</h1>");
+
+            await pdf.SaveAsAsync("c:\\my.pdf");
+            
+            var filePath = await pdf.SaveInTempFolderAsync();
+
+            Console.WriteLine(filePath);
+            
+            Console.ReadLine();
+        }
+
+        static async Task PrintWithImageFromFile()
+        {
+            var renderer = new HtmlToPdf();
+
+            renderer.PrintOptions.Title = "My title";
+            
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "files", "GitHub_Logo.png");
+            
+            var pdf = await renderer.RenderHtmlAsPdfAsync($"<img src=\"{path}\"><h1>Hello world</h1>");
 
             await pdf.SaveAsAsync("c:\\my.pdf");
             
